d025a677185af64c2d13e1fa85627c2255ec0f40
jotika04 <jotika.adhisthana@gmail.com>
2021-04-09 15:33:19 +0700

    add docz

---
+++ webshowcase-frontend/.docz/.cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("D:\\webshowcase-frontend\\webshowcase-frontend\\.docz\\.cache\\dev-404-page.js"))),
+  "component---doczmdxfiles-login-mdx": (preferDefault(require("D:\\webshowcase-frontend\\webshowcase-frontend\\doczmdxfiles\\login.mdx"))),
+  "component---readme-md": (preferDefault(require("D:\\webshowcase-frontend\\webshowcase-frontend\\README.md"))),
+  "component---src-pages-404-js": (preferDefault(require("D:\\webshowcase-frontend\\webshowcase-frontend\\.docz\\src\\pages\\404.js")))
+}
+
