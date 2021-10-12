5d442b508fbf24d70471a02bebe03fa625b60bef
jotika04 <jotika.adhisthana@gmail.com>
2021-10-12 23:46:34 +0700

    commit3

--- webshowcase/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("D:\\webshowcase-frontend-combined\\webshowcase\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("D:\\webshowcase-frontend-combined\\webshowcase\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("D:\\webshowcase-frontend-combined\\webshowcase\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("D:\\webshowcase-frontend-combined\\webshowcase\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\webshowcase-frontend-combined\\webshowcase\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\webshowcase-frontend-combined\\webshowcase\\src\\css\\custom.css"),
-];
